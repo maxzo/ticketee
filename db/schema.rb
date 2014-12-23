f1b996af910c2b72d85ed4b57e7d2e776f9d2296
maxzo <sariego.isi@gmail.com>
2014-12-23 11:56:16 -0300

    Added admin flag to user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140921171746) do
+ActiveRecord::Schema.define(version: 20141223144416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20140921171746) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name "example_user"
-    email "example@example.com"
+    name "username"
+    email "sample@example.com"
     password "hunter2"
     password_confirmation "hunter2"
+
+    factory :admin_user do
+      admin true
+    end
   end
 end
