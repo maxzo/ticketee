84cfa0bade05f6c2244a9f6aaf09a6d2dee77622
maxzo <sariego.isi@gmail.com>
2015-01-16 15:09:53 -0300

    Add ability to edit and update users.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,12 +1,14 @@
 FactoryGirl.define do
+  sequence(:email) {|n| "user#{n}@example.com" }
+
   factory :user do
-    name "example_user"
-    email "example@example.com"
+    name "username"
+    email { generate(:email) }
     password "hunter2"
-    password_confirmation "hunter2"
 
     factory :admin_user do
       admin true
     end
   end
 end
+
